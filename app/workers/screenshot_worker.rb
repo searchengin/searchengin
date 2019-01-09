889207c6a785db255b388df1048e26fec919e959
rorong <rordev@ongraph.com>
2019-01-09 14:22:43 +0530

    small fix

--- app/workers/screenshot_worker.rb
+++ app/workers/screenshot_worker.rb
@@ -1,25 +1,28 @@
 class ScreenshotWorker
   include Sidekiq::Worker
+  require 'webshot'
 
-  # def perform(url)
+  # def perform(url_id)
+  #   begin
   #   # Do something
-  #   url = Url.find_by(url)
+  #   url = Url.find_by(id: url_id)
+  #   link = url.url.split('://')
+  #   domain_list = link.second.split('.').first
   #   puts "Capture screenshot by sidekiq >>>>>"
   #   # encoded_url = Base64.urlsafe_encode64(url)
-  #   binding.pry
   #   ws = Webshot::Screenshot.instance
-  #   screenshot = ws.capture url.url, "#{domain_list[0]}.png"
+  #   screenshot = ws.capture url.url, "#{domain_list}.png"
   #   thumb = MiniMagick::Image.open(screenshot.path)
-  #   thumb.write "#{domain_list[0]}.png"
+  #   # img = thumb.write "#{domain_list}.png"
 
   #   url.update(screenshot: thumb)
   #   File.delete "#{domain_list[0]}.png"
+  #   rescue Exception
+  #   end
   # end
 
-  def perform(url_id, domain_list)
-    puts "Capture screenshot by sidekiq >>>>>"
-    Url.screenshot_capture url_id, domain_list
-  end
-
-
+  # def perform(url_id, domain_list)
+  #   puts "Capture screenshot by sidekiq >>>>>"
+  #   Url.screenshot_capture url_id, domain_list
+  # end
 end
