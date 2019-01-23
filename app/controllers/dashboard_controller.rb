fa3ccc7e186c996736e98bd62b70f89e5caeef4e
rorong <rordev@ongraph.com>
2019-01-23 12:51:19 +0530

    Search Module functionality

--- app/controllers/dashboard_controller.rb
+++ app/controllers/dashboard_controller.rb
@@ -1,5 +1,7 @@
 class DashboardController < ApplicationController
   # before_action :authenticate_user!
+include Dnsruby
+
 
   def index
     @urls = Url.all.order("created_at DESC")
@@ -23,30 +25,174 @@ class DashboardController < ApplicationController
   end
 
   def search
-    search = params[:search]
-    # if params[:search]
-    # binding.pry
-    #   @results = WhatDatum.where("description = ? OR website_url = ? OR item_name ILIKE (?)",params[:search], params[:search], params[:search])
-    #   what_ids = @results.pluck(:what_tag_id)
-    #  what_tags = Tag.find
-    # end
-
-    #Tag.joins(:what_tags).where("who_datum.description = ? OR who_datum.website_url  ILIKE (?)",params[:search], params[:search])
-    #@tag = Tag.joins(:what_datum, :who_datum).where("what_data.description = ? OR what_data.website_url  ILIKE (?)", params[:search], params[:search]).where("account = ? OR description  ILIKE (?)",params[:search], params[:search])
-    #redirect_to dashboard_result_path
-  #end
-  @urls_data = {}
-  @tag  = Tag.joins(:where_datum).where("location = ? OR description ILIKE (?)",params[:search], params[:search])
-  # @tag = Tag.joins(:what_datum).where("what_data.description = ? OR what_data.website_url  ILIKE (?)", params[:search], params[:search]) || Tag.joins(:who_datum).where("account = ? OR description  ILIKE (?)",params[:search], params[:search]) || Tag.joins(:when_datum).where("description ILIKE (?)", params[:search]) || Tag.joins(:where_datum).where("location = ? OR description ILIKE (?)",params[:search], params[:search]) || Tag.joins(:why_datum).where("title = ? OR description ILIKE (?)", params[:search],params[:search])
-
-  if @tag.first
-    tag_id = @tag.first.id
-    first_tag = Tag.find(tag_id)
-    @urls_data[first_tag.url] = [first_tag]
-  end
-
-  url = Url.all.where("domain = ? AND subdomain = ? AND like ILIKE (?)", params[:search],params[:search], params[:search])
-
+    @urls_data = {}
+    limit = 6
+    offset = params[:page].to_i * limit
+    @query = params[:search]
+    connection = ActiveRecord::Base.connection
+    unless params[:page]
+      limit = 15
+      first_tag = connection.exec_query("select t.id, t.type from tags as t
+                left join where_data as where_data on where_data.where_tag_id = t.id
+                left join what_data as what_data on what_data.what_tag_id = t.id
+                left join when_data as when_data on when_data.when_tag_id = t.id
+                left join who_data as who_data on who_data.who_tag_id = t.id
+                left join why_data as why_data on why_data.why_tag_id = t.id
+                where t.verified = 'true' or
+                (
+                  what_data.description ilike '%#{@query}%' or
+                  what_data.website_url ilike '%#{@query}%' or
+                  what_data.item_name ilike '%#{@query}%' or
+                  where_data.location ilike '%#{@query}%' or
+                  where_data.description ilike '%#{@query}%' or
+                  when_data.description ilike '%#{@query}%' or
+                  who_data.account ilike '%#{@query}%' or
+                  who_data.description ilike '%#{@query}%' or
+                  why_data.title ilike '%#{@query}%' or
+                  why_data.description ilike '%#{@query}%'
+                )
+                order by points desc
+                limit 1").first
+      if first_tag
+        first_tag = Tag.find(first_tag['id'])
+        @urls_data[first_tag.url] = [first_tag]
+      end
+      urls = connection.exec_query("with query_filter as (select u.url, u.points, u.title, u.code,
+            case
+              when domain ilike '%#{@query}%' then 300
+              when subdomain ilike '%#{@query}%' then 150
+              when url ilike '%#{@query}%' then 100
+              else 0
+            end as extra_points,
+            case
+              when domain ilike '%#{@query}%' then 300 + points
+              when subdomain ilike '%#{@query}%' then 150 + points
+              when url ilike '%#{@query}%' then 100 + points
+              else points
+            end as sum_points
+             from urls as u
+             where url ilike '%#{@query}%' or title ilike '%#{@query}%' or description ilike '%#{@query}%'
+             )
+            (select u.url, u.points, u.title, u.code,  extra_points, sum_points, count(w) as inbound_links
+            from query_filter as u
+            left outer join what_data as w
+              on u.url = w.website_url and w.category = 'External Link'
+            group by u.url, u.points, u.title, u.code,  extra_points, sum_points
+            order by extra_points desc, inbound_links desc, points desc
+            limit 2) union all (
+            select u.url, u.points, u.title, u.code, extra_points, sum_points, count(w) as inbound_links
+            from query_filter as u
+            left outer join what_data as w
+              on u.url = w.website_url and w.category = 'External Link'
+            group by u.url, u.points, u.title, u.code, extra_points, sum_points
+            order by sum_points desc, inbound_links desc, points desc
+          ) limit #{limit} offset #{offset}"
+        )
+      #urls_urls = urls.map {|url| url[:url]}
+      urls_urls =  urls.rows.collect {|ind| ind[0]}
+      @urls = Url.where(:url => urls_urls).all
+      @urls.each do |k|
+        tags =  connection.exec_query("
+                select t.id, t.type from tags as t
+                left join what_data as what_data on what_data.what_tag_id = t.id
+                left join where_data as where_data on where_data.where_tag_id = t.id
+                left join when_data as when_data on when_data.when_tag_id = t.id
+                left join who_data as who_data on who_data.who_tag_id = t.id
+                left join why_data as why_data on why_data.why_tag_id = t.id
+                where t.url_id = #{k.id} and
+                (
+                  what_data.description ilike '%#{@query}%' or
+                  what_data.website_url ilike '%#{@query}%' or
+                  what_data.item_name ilike '%#{@query}%' or
+                  where_data.location ilike '%#{@query}%' or
+                  where_data.description ilike '%#{@query}%' or
+                  when_data.description ilike '%#{@query}%' or
+                  who_data.account ilike '%#{@query}%' or
+                  who_data.description ilike '%#{@query}%' or
+                  why_data.title ilike '%#{@query}%' or
+                  why_data.description ilike '%#{@query}%'
+                ) and
+                (
+                  t.verified = 'false'
+                )
+                order by points desc
+                limit 3
+              ")
+      end
+    else
+       urls = connection.exec_query("
+            with query_filter as (
+              select u.url, u.points, u.title, u.code, case
+                  when domain ilike '%#{@query}%' then 300
+                  when subdomain ilike '%#{@query}%' then 150
+                  when url ilike '%#{@query}%' then 100
+                  else 0
+                end as extra_points,
+                case
+                  when domain ilike '%#{@query}%' then 300 + points
+                  when subdomain ilike '%#{@query}%' then 150 + points
+                  when url ilike '%#{@query}%' then 100 + points
+                  else points
+                end as sum_points
+              from urls as u
+              where url ilike '%#{@query}%' or title ilike '%#{@query}%' or description ilike '%#{@query}%'
+            ) (
+              select u.url, u.points, u.title, u.code, extra_points, sum_points, count(w) as inbound_links
+              from query_filter as u
+              left outer join what_tags as w
+                on u.url = w.website_url and w.category = 'External Link'
+              group by u.url, u.points, u.title, u.code, extra_points, sum_points
+              order by extra_points desc, inbound_links desc, points desc
+              limit 2
+            ) union all (
+              select u.url, u.points, u.title, u.code, extra_points, sum_points, count(w) as inbound_links
+              from query_filter as u
+              left outer join what_tags as w
+                on u.url = w.website_url and w.category = 'External Link'
+              group by u.url, u.points, u.title, u.code, extra_points, sum_points
+              order by sum_points desc, inbound_links desc, points desc
+            ) limit #{limit} offset #{offset}
+          ")
+          # tags with query
+          urls_urls = urls.map {|url| url[:url]}
+          urls = Url.where(:url => urls_urls).all
+          urls.each_with_index do |url, index|
+            # url = Url.find(url: url[:url])
+            tags =  connection.exec_query("
+              select t.id, t.kind, t.verified from tags as t
+              left join what_data as what_data on what_data.what_tag_id = t.id
+              left join where_data as where_data on where_data.where_tag_id = t.id
+              left join when_data as when_data on when_data.when_tag_id = t.id
+              left join who_data as who_data on who_data.who_tag_id = t.id
+              left join why_data as why_data on why_data.why_tag_id = t.id
+              where t.url_id = #{1} and
+              (
+                what_data.description ilike '%#{@query}%' or
+                what_data.website_url ilike '%#{@query}%' or
+                what_data.item_name ilike '%#{@query}%' or
+                where_data.location ilike '%#{@query}%' or
+                where_data.description ilike '%#{@query}%' or
+                when_data.description ilike '%#{@query}%' or
+                who_data.account ilike '%#{@query}%' or
+                who_data.description ilike '%#{@query}%' or
+                why_data.title ilike '%#{@query}%' or
+                why_data.description ilike '%#{@query}%'
+              ) and
+              (
+                t.verified = 'false'
+              )
+              order by points desc
+              limit 3
+            ")
+            @urls_data[url] = []
+            tags.all.each do |tag|
+              tag = Tag.find(id: tag[:id])
+              @urls_data[url].append(tag)
+            end
+          end
+        end
+      @title = "\"#{@query}\" Search Results | "
+      @urls_data
   end
 
   def like
@@ -77,5 +223,24 @@ class DashboardController < ApplicationController
     end
       @dislike_count = Dislike.where(url_id: params[:url]).count
   end
+
+
+  # def verify_domain
+  #   #username = params[:username]
+  #   #user = User.find(username: @username)
+
+  #   url = Url.find(params[:id])
+  #   domain = url.domain
+  #   #mandrill = Mandrill::API.new ENV['MANDRILL_APIKEY']
+  #   #res = Resolver.new
+  #   #ret = res.query(url)
+  #   res = Dnsruby::Resolver.new
+  #   ret = res.query(domain, Types.TXT)
+  #   p ret.answer
+
+  #   Dnsruby::DNS.open {|dns|
+  #     dns.each_resource(domain, Types.A) {|r| p r}
+  #   }
+  # end
 
 end
