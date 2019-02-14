7169e351144b90b32b8e6d78ffbf3bce17cc9d57
rorong <rordev@ongraph.com>
2019-02-14 19:02:17 +0530

    change to avoid code break

--- db/seeds.rb
+++ db/seeds.rb
@@ -6,7 +6,20 @@
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
 
-  user = User.new(username: "admin", email: "admin@searcheng.in", password: "admin1234", password_confirmation: "admin1234", first_name: "Admin", last_name: "Admin")
-  user.save(validate: false)
-  user.add_role :superadmin
+  if Role.find_by(name: :superadmin).nil?
+    user_sadmin = User.new(username: "superadmin", email: "superadmin@searcheng.in", password: "superadmin1234", password_confirmation: "superadmin1234", first_name: "Super", last_name: "Admin")
+    user_sadmin.save(validate: false)
+    user_sadmin.add_role :superadmin
+  end
 
+  if Role.find_by(name: :admin).nil?
+    user_admin = User.new(username: "admin", email: "admin@searcheng.in", password: "admin1234", password_confirmation: "admin1234", first_name: "Admin", last_name: "Admin")
+    user_admin.save(validate: false)
+    user_admin.add_role :editor
+  end
+
+  if Role.find_by(name: :editor).nil?
+    user_editor = User.new(username: "editor", email: "editor@searcheng.in", password: "editor1234", password_confirmation: "editor1234", first_name: "Editor", last_name: "Editor")
+    user_editor.save(validate: false)
+    user_editor.add_role :editor
+  end
