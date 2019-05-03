f3f6e4ab96b8e8fd38cc71eef72844a84c233ba6
rorong <rordev@ongraph.com>
2019-05-03 18:22:17 +0530

    Small change for tag limit in url page

--- app/helpers/urls_helper.rb
+++ app/helpers/urls_helper.rb
@@ -2,6 +2,6 @@ module UrlsHelper
 
   def limit_tags url
     #url.tags.where(verified: 'true').count == 3 ? url.tags.where(verified: 'true') : url.tags.order(created_at: 'DESC').limit(3)
-    url.tags.where(verified: false).order(created_at: 'DESC').limit(3)
+    url.tags.order(created_at: 'DESC').limit(3)
   end
 end
